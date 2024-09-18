a973cccc3a73afcd8cf9b2851dbad2f9f52ca12c
aiesecegypt <egypt@aiesec.net>
2024-09-18 19:07:51 +0300

    timestamp2

--- RPM_Backend/urls.py
+++ RPM_Backend/urls.py
@@ -20,12 +20,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
-from rpm.schema import schema
+from .schema import schema
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rpm.urls')),
     path('', include('partners.urls')),
-    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG == False:
