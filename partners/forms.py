6e7fffdae711c056bb0d40c9e69eab808829297a
aiesecegypt <egypt@aiesec.net>
2024-09-22 12:29:46 +0300

    SETTINGs

--- partners/forms.py
+++ partners/forms.py
@@ -5,4 +5,6 @@ class PartnerForm(forms.ModelForm):
     class Meta:
         model = Partner
         fields = ['company_name','company_field','company_size','linkedin',
-                  'company_website','why','contact_name','contact_mail','contact_phone','contact_position']
+                  'company_website','why','contact_name','contact_mail','country_code2','contact_phone','contact_position']
+        
+        country_code2 = forms.CharField(widget=forms.HiddenInput())  # Hidden field for country code
--- partners/models.py
+++ partners/models.py
@@ -17,6 +17,8 @@ class Partner(models.Model):
     contact_mail = models.EmailField(max_length=255)
     contact_phone = models.CharField(max_length=255)
     contact_position = models.CharField(max_length=255)
+    country_code2 = models.CharField(max_length=3,default=+20)  # Adjust max_length if needed
+
 
     def __str__(self):
         return self.company_name
